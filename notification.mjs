7957b84e4cd281f783c0894c23b2507b878b9b10
yuttasartviratpan <yuttasart.vir@student.mahidol.edu>
2023-11-19 00:31:25 +0700

    Fix env

--- notification.mjs
+++ notification.mjs
@@ -20,7 +20,7 @@ redis.on("connect", () => {
   console.log("Connected to Redis");
 });
 
-const client = createClient();
+const client = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` });
 client.on("error", (err) => console.log("Redis Client Error", err));
 await client.connect();
 
